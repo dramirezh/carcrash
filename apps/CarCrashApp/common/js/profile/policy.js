a61ef53350ff931ceed53bae8da57bd8a1a3dc7c
leo <leo@leo-HP>
2015-06-26 12:10:32 -0500

    updated vehicle inputs

--- apps/CarCrashApp/common/js/profile/policy.js
+++ apps/CarCrashApp/common/js/profile/policy.js
@@ -205,6 +205,9 @@
 		basicPersonFiltersNumber("txtPolContactCellPhone");        	 
    	 basicPersonNameFilters("txtPolContactFirstName");
    	validEmail("txtPolContactEmail");
+   	validateAlphaNumeric("searchSubMark");
+   	validateAlphaNumeric("txtSeries");
+   	validatePolicyNumber("txtPolicyNo");
    	
    	 $('#txtPolContactName').keypress(function(key) {        		
    		 if((key.charCode < 97 || key.charCode > 122) && (key.charCode < 65 || key.charCode > 90) && (key.charCode != 32)
@@ -1142,3 +1145,30 @@ function initPolicyToList(name,insurance,policyDate,id,pic){
     		}
 			
 		}
+		
+		function validateAlphaNumeric(inputName){
+			 $('#'+inputName).keypress(function(key) {        		
+				 if((key.charCode < 97 || key.charCode > 122) && (key.charCode < 65 || key.charCode > 90) && (key.charCode != 32)
+						 && (key.charCode != 225)&& (key.charCode != 233)&& (key.charCode != 237)&& (key.charCode != 243)&& (key.charCode != 250)&& (key.charCode != 241)
+			   				&& (key.charCode != 193)&& (key.charCode != 201)&& (key.charCode != 205)&& (key.charCode != 211)&& (key.charCode != 218)&&  (key.charCode != 209)	
+			   				&& (key.charCode < 48 || key.charCode > 57)
+				 ){
+		    		 return false;
+		    	 }else{             		
+		    	        return true;
+		    	    }
+				});
+		 }
+		function validatePolicyNumber(inputName){
+			 $('#'+inputName).keypress(function(key) {        		
+				 if((key.charCode < 97 || key.charCode > 122) && (key.charCode < 65 || key.charCode > 90)
+						 && (key.charCode != 225)&& (key.charCode != 233)&& (key.charCode != 237)&& (key.charCode != 243)&& (key.charCode != 250)&& (key.charCode != 241)
+			   				&& (key.charCode != 193)&& (key.charCode != 201)&& (key.charCode != 205)&& (key.charCode != 211)&& (key.charCode != 218)&&  (key.charCode != 209)	
+			   				&& (key.charCode < 48 || key.charCode > 57)
+				 ){
+		    		 return false;
+		    	 }else{             		
+		    	        return true;
+		    	    }
+				});
+		 }
