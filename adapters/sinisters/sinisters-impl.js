81c88d5b43f5ea13192801fd8253c22969a9fec3
leo <leo@leo-HP>
2015-07-23 09:50:49 -0500

    sinister http adapter

--- adapters/sinisters/sinisters-impl.js
+++ adapters/sinisters/sinisters-impl.js
@@ -1,80 +1,34 @@
-/*******************************************************************************
- * Functions that correspond to JSONStore client operations
- * 
- */
-
-var selectStatement = WL.Server.createSQLStatement(
-	"SELECT R.Identifier AS identifier, R.Email AS email, R.IDInsurancePolicy AS idPolicy, LEFT(CONVERT(varchar, R.[Date], 120) ,10) AS date, " +
-	"R.[Time] as [time], R.[Type] AS [type], R.[Status] AS [status], R.Lat AS lat, R.Lng AS lng, " +
-	"RE.MedicalAssistance as medicalAssistance, RE.LegalAssistance as legalAssistance, RE.CraneService as craneService " +
-	"FROM Reports R " +
-	"LEFT JOIN ReportsExtra RE ON RE.Identifier = R.Identifier AND RE.Email = R.Email " +
-	"WHERE R.Email = ?"
-);
-var selectPicsStatement = WL.Server.createSQLStatement("SELECT PictureURL AS url from ReportPictures WHERE Identifier = ? AND Email = ?");
-
-var addStatement = WL.Server.createSQLStatement(
-	"INSERT INTO Reports (Identifier, Email, IDInsurancePolicy, [Date], [Time], [Type], [Status], Lat, Lng) " +
-	"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-);
-var addExtrasStatement = WL.Server.createSQLStatement(
-	"INSERT INTO ReportsExtra (Identifier, Email, Severity) " +
-	"VALUES(?,?,?)"
-);
-var addPicsStatement = WL.Server.createSQLStatement(
-	"INSERT INTO ReportPictures (Identifier, Email, PictureURL) " +
-	"VALUES(?,?,?)"
-);
-var updateStatement = WL.Server.createSQLStatement(
-	"UPDATE Reports SET Status = ? WHERE Identifier = ? AND Email = ?"
-);
-
-var deleteStatement = WL.Server.createSQLStatement(
-	"DELETE FROM Reports WHERE Identifier = ? AND Email = ?;"
-);
-var deleteExtrasStatement = WL.Server.createSQLStatement(
-	"DELETE FORM ReportsExtra WHERE Identifier = ? AND Email = ?"
-);
-var deletePicsStatement = WL.Server.createSQLStatement(
-	"DELETE FORM ReportPictures WHERE Identifier = ? AND Email = ?"
-);
-
 function getSinisters(oData) {
-	var result = WL.Server.invokeSQLStatement({
-		preparedStatement : selectStatement,
-		parameters : [oData.email]
-	});
 	
+	var input = {
+		    method : 'get',
+		    returnedContentType : 'json',
+		    path :'/GoShieldServices/goshield.svc/Sinisters/Get?Email='+oData.email
+		};
+		var result = WL.Server.invokeHttp(input);	
+		
 	var oReturn = [];
-	for(var i = 0; i < result.resultSet.length; i++){
+	for(var i = 0; i < result.getSinisterResult.length; i++){
 		var data = {
-					identifier: result.resultSet[i].identifier,
-					email : result.resultSet[i].email,
-					idPolicy: result.resultSet[i].idPolicy,
-					date: result.resultSet[i].date,
-					time: result.resultSet[i].time,
-					type: result.resultSet[i].type,
-					status: result.resultSet[i].status,
+					identifier: result.getSinisterResult[i].identifier,
+					email : result.getSinisterResult[i].email,
+					idPolicy: result.getSinisterResult[i].idPolicy,
+					date: result.getSinisterResult[i].date,
+					time: result.getSinisterResult[i].time,
+					type: result.getSinisterResult[i].type,
+					status: result.getSinisterResult[i].status,
 					location: {
-						lat: result.resultSet[i].lat,
-						lng: result.resultSet[i].lng
+						lat: result.getSinisterResult[i].lat,
+						lng: result.getSinisterResult[i].lng
 					},
 					extras: {
-						pictures: [],
-						medicalAssistance: result.resultSet[i].medicalAssistance,
-						legalAssistance: result.resultSet[i].legalAssistance,
-						craneService: result.resultSet[i].craneService,
-						comments: result.resultSet[i].comments
+						pictures: result.getSinisterResult[i].data.extras.pictures,
+						medicalAssistance: result.getSinisterResult[i].medicalAssistance,
+						legalAssistance: result.getSinisterResult[i].legalAssistance,
+						craneService: result.getSinisterResult[i].craneService,
+						comments: result.getSinisterResult[i].comments
 					}
 				};
-		var pics = WL.Server.invokeSQLStatement({
-			preparedStatement : selectPicsStatement,
-			parameters : [result.resultSet[i].identifier, result.resultSet[i].email]
-		});
-		for(var a = 0; a < pics.resultSet.length; a++){
-			data.extras.pictures.push(pics.resultSet[a]);
-		}
-		
 		oReturn.push(data);
 	}
 	return {data: oReturn};
@@ -101,51 +55,57 @@ function saveSinisters(param1) {
 }
 
 function save(sinister){
-	var a, b, c = {};
-	a = WL.Server.invokeSQLStatement({
-		preparedStatement : addStatement,
-		parameters : [sinister.identifier, sinister.email, sinister.idPolicy, sinister.date, sinister.time, sinister.type, sinister.status + 1, sinister.location.lat, sinister.location.lng]
-	});
-	if(sinister.type != "theft"){
-		b = WL.Server.invokeSQLStatement({
-			preparedStatement : addExtrasStatement,
-			parameters : [sinister.identifier, sinister.email, sinister.severity]
-		});
+	
+		var data = {
+					
+					idPolicy: sinister.idPolicy,
+					date: sinister.date,
+					time: sinister.time,
+					type: sinister.type,
+					status: sinister.status,
+					location: sinister.location,
+					extras: sinister.extras
+				};
 		
-		c = [];
-		for(var i = 0; i < sinister.extras.pictures.length; i++){
-			 c.push(WL.Server.invokeSQLStatement({
-				preparedStatement : addPicsStatement,
-				parameters : [sinister.identifier, sinister.email, sinister.extras.pictures[i].url]
-			}));
-		}
-		return [ {"a":a},{"b":b},{"c":c}];
-	}
-	else{
-		return a;
-	}
+	
+	var inputData= {data: data,
+			identifier: sinister.identifier,
+			email : sinister.email,			
+	};
+	
+	var data=input('post','json','/GoShieldServices/goshield.svc/Sinisters/Save','application/json; charset=UTF-8',JSON.stringify(inputData));		
+	 WL.Server.invokeHttp(data);
+	 return WL.Server.invokeHttp(data);
 }
+
 function update(sinister){
-	return WL.Server.invokeSQLStatement({
-		preparedStatement : updateStatement,
-		parameters : [sinister.identifier, sinister.email, sinister.status]
-	});
+	var inputData={
+			identifier:sinister.identifier,
+			email:sinister.email,
+			status:sinister.status			
+	};
+	var data=input('post','json','/GoShieldServices/goshield.svc/Sinisters/Update','application/json; charset=UTF-8',JSON.stringify(inputData));		
+	return WL.Server.invokeHttp(data);
 }
+
 function remove(sinister){
-	var a = WL.Server.invokeSQLStatement({
-		preparedStatement : deleteStatement,
-		parameters : [sinister.identifier, sinister.email]
-	});
+	var inputData={
+			identifier:sinister.identifier,
+			email:sinister.email		
+	};
+	var data=input('post','json','/GoShieldServices/goshield.svc/Sinisters/Remove','application/json; charset=UTF-8',JSON.stringify(inputData));		
+	return WL.Server.invokeHttp(data);				 
+}
+
+function input( pMethod, pReturnedContentType,pPath, pContentType, pContent){
 	
-	var b = WL.Server.invokeSQLStatement({
-		preparedStatement : deleteExtrasStatement,
-		parameters : [sinister.identifier, sinister.email]
-	});
-	
-	var c = WL.Server.invokeSQLStatement({
-		preparedStatement : deletePicsStatement,
-		parameters : [sinister.identifier, sinister.email]
-	});
-	
-	return [a,b,c];
+	return data = {
+		    method : pMethod,
+		    returnedContentType :pReturnedContentType,
+		    path :pPath,
+		    body: { 
+		    	   		contentType: pContentType , 
+		    	   		content: pContent
+		    	   	} 
+		};
 }
